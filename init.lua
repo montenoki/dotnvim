01f8ad9339e63e400b563952c4a9f24f9f3c2e38
monten.oki <monten.oki@gmail.com>
2022-05-14 09:33:12 +0900

    Packer plugins

--- init.lua
+++ init.lua
@@ -3,3 +3,6 @@ require("basic")
 
 -- キーバインド
 require("keybindings")
+
+-- 拡張機能
+require("plugins")
---
+++ lua/plugins.lua
@@ -0,0 +1,25 @@
+local packer = require("packer")
+packer.startup({
+  function(use)
+    -- 拡張機能マネージャー
+    use 'wbthomason/packer.nvim'
+  end,
+  config = {
+    display = {
+      open_fn = function()
+        return require("packer.util").float({ border = "single" })
+      end,
+    },
+  }
+})
+
+--  plugins.lua ファイル保存時に自動更新
+pcall(
+  vim.cmd,
+  [[
+    augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+  ]]
+)
